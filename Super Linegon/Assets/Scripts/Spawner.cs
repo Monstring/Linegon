1c0820cf700fdb2e76f65001eef9bf6ea7182a65
Monstring <monstring1@hotmail.com>
2018-02-06 17:00:45 +0300

    -Created another class that controls the scale of the spawned walls

--- Super Linegon/Assets/Scripts/Spawner.cs
+++ Super Linegon/Assets/Scripts/Spawner.cs
@@ -7,19 +7,26 @@ public class Spawner : MonoBehaviour {
     public GameObject walls;
     public int lvl = 1;
 
-    public void OnDrawGizmos()
-    {
-        Vector3 screenSize = Camera.main.ViewportToWorldPoint(new Vector3(1,1,1));
-        Gizmos.DrawWireCube(new Vector3(4,6.5f,0), new Vector3(screenSize.x, 1, 0));
-    }
+    public float width = 8;
+    public float height = 1;
+
 
     // Use this for initialization
     void Start () {
-        Instantiate(walls, transform.position, Quaternion.identity);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update () {
 
 	}
+
+    void Spawn()
+    {
+
+        for (int i = 0; i < lvl; i++)
+        {
+            Instantiate(walls, transform.position, Quaternion.identity);
+        }
+    }
 }
---
+++ Super Linegon/Assets/SizeScaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeScaler : MonoBehaviour {
+
+    Spawner spawn = new Spawner();
+
+	// Use this for initialization
+	void Start () {
+        transform.localScale = new Vector3(spawn.width,spawn.height,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
